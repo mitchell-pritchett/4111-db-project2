1bd37aae16fd3bb3754869ed185ff786d3806dda
cluess_coder <clementinesandeggs@gmail.com>
2022-12-03 22:22:37 -0500

    project 2 first commit

---
+++ add_storyline(1-16).sql
@@ -0,0 +1,48 @@
+UPDATE Contents
+SET storyline = 'Unlucky assassin Ladybug (Brad Pitt) is determined to do his job peacefully after one too many gigs has gone off the rails. Fate has other plans, however: Ladybug''s latest mission puts him on a collision course with lethal adversaries from around the globe--all with connected, yet conflicting, objectives--on the world''s fastest train. The end of the line is just the beginning in this non-stop thrill-ride through modern-day Japan.'
+WHERE conid = 1;
+UPDATE Contents
+SET storyline = 'Reawakening after 5000 years, Black Adam becomes the world''s ruthless protector: an anti-villain to take on the likes of Superman and Wonder Woman. Now in the 21st-Century, Black Adam must face off against the Justice Society of America and its heroes: Doctor Fate, Hawkman, Atom Smasher and Cyclone. The fate of the world hangs in the balance.'
+WHERE conid = 2;
+UPDATE Contents
+SET storyline = 'Thor''s retirement is interrupted by a galactic killer known as Gorr the God Butcher, who seeks the extinction of the gods. To combat the threat, Thor enlists the help of King Valkyrie, Korg and ex-girlfriend Jane Foster, who - to Thor''s surprise - inexplicably wields his magical hammer, Mjolnir, as the Mighty Thor. Together, they embark upon a harrowing cosmic adventure to uncover the mystery of the God Butcher''s vengeance and stop him before it''s too late.'
+WHERE conid = 3;
+UPDATE Contents
+SET storyline = 'With her laundromat teetering on the brink of failure and her marriage to wimpy husband Waymond on the rocks, overworked Evelyn Wang struggles to cope with everything, including a tattered relationship with her judgmental father Gong Gong and Joy, her daughter. And, as if facing a gloomy midlife crisis weren''t enough, Evelyn must brace herself for an unpleasant meeting with an impersonal bureaucrat: Deirdre, the shabbily dressed IRS auditor. However, as the stern agent loses patience, an inexplicable multiverse rift becomes an eye-opening exploration of parallel realities. Will Evelyn jump down the rabbit hole? How many stars are there in the universe? Can weary Evelyn fathom the irrepressible force of possibilities, tap into newfound powers, and prevent an evil entity from destroying the thin, countless layers of the unseen world?'
+WHERE conid = 4;
+UPDATE Contents
+SET storyline = 'Follows the title reptile who lives in a house on East 88th Street in New York City. Lyle enjoys helping the Primm family with everyday chores and playing with the neighborhood kids but one neighbor insists that Lyle belongs in a zoo. Mr. Grumps and his cat, Loretta, do not like crocodiles, and Lyle tries to prove that he is not as bad as others might first think.'
+WHERE conid = 5;
+UPDATE Contents
+SET storyline = 'When the Justice League are captured by Lex Luthor, Superman''s dog, Krypto, forms a team of shelter-pets who were given super-powers: A hound named Ace, who becomes super-strong, a pig named PB, who can grow to giant-size, a turtle named Merton, who becomes super-fast, and a squirrel named Chip, who gains electric-powers.'
+WHERE conid = 6;
+UPDATE Contents
+SET storyline = 'From director Andrew Dominik, and based on the bestselling novel by Joyce Carol Oates, "Blonde" boldly reimagines the life of one of Hollywood''s most enduring icons, Marilyn Monroe. From her volatile childhood as Norma Jeane, through her rise to stardom and romantic entanglements, "Blonde" blurs the lines of fact and fiction to explore the widening split between her public and private selves.'
+WHERE conid = 7;
+UPDATE Contents
+SET storyline = 'Elvis follows the story of infamous rock ''n'' roll star Elvis Presley seen through the eyes of his controversial manager Colonel Tom Parker. The film explores Elvis Presley''s highs and lows and the many challenges and controversies he received throughout his career.'
+WHERE conid = 8;
+UPDATE Contents
+SET storyline = '"Amsterdam," the latest film from acclaimed writer/director David O. Russell, is a fascinating and richly intricate tale that brilliantly weaves historical fact with fiction for a timely, cinematic experience. 20th Century Studios'' and New Regency''s original crime epic is about three close friends who find themselves at the center of one of the most shocking secret plots in American history.'
+WHERE conid = 9;
+UPDATE Contents
+SET storyline = 'Two young women accidentally bring back the Sanderson Sisters to modern-day Salem and must figure out how to stop the child-hungry witches from wreaking havoc on the world. The world may have been free of Winifred, Mary, and Sarah and the fear they invoked in us all, but what happens when you mess with magic? It messes back.'
+WHERE conid = 10;
+UPDATE Contents
+SET storyline = 'A massive storm rages. A young girl is kidnapped. Her mother, with no other option, teams up with the mysterious older woman next door to pursue the kidnapper a journey into the wilderness that will test their limits and expose dark and shocking secrets from their pasts.'
+WHERE conid = 11;
+UPDATE Contents
+SET storyline = 'Jamie Lee Curtis returns to her iconic role as Laurie Strode, who comes to her final confrontation with Michael Myers (Nick Castle and James Jude Courtney), the masked figure who has haunted her since she narrowly escaped his killing spree on Halloween night four decades ago.'
+WHERE conid = 12;
+UPDATE Contents
+SET storyline = 'The Targaryen dynasty is at the absolute apex of its power, with more than 10 dragons under their yoke. Most empires crumble from such heights. In the case of the Targaryens, their slow fall begins when King Viserys breaks with a century of tradition by naming his daughter Rhaenyra heir to the Iron Throne. But when Viserys later fathers a son, the court is shocked when Rhaenyra retains her status as his heir, and seeds of division sow friction across the realm.'
+WHERE conid = 13;
+UPDATE Contents
+SET storyline = 'This epic drama is set thousands of years before the events of J.R.R. Tolkien’s The Hobbit and The Lord of the Rings, and will take viewers back to an era in which great powers were forged, kingdoms rose to glory and fell to ruin, unlikely heroes were tested, hope hung by the finest of threads, and the greatest villain that ever flowed from Tolkien’s pen threatened to cover all the world in darkness. Beginning in a time of relative peace, the series follows an ensemble cast of characters, both familiar and new, as they confront the long-feared re-emergence of evil to Middle-earth. From the darkest depths of the Misty Mountains, to the majestic forests of the elf-capital of Lindon, to the breathtaking island kingdom of Númenor, to the furthest reaches of the map, these kingdoms and characters will carve out legacies that live on long after they are gone.'
+WHERE conid = 14;
+UPDATE Contents
+SET storyline = 'Jennifer Walters navigates the complicated life of a single, 30-something attorney who also happens to be a green 6-foot-7-inch superpowered Hulk.'
+WHERE conid = 15;
+UPDATE Contents
+SET storyline = 'The series will explore a new perspective from the Star Wars galaxy, focusing on Cassian Andor''s journey to discover the difference he can make. The series brings forward the tale of the burgeoning rebellion against the Empire and how people and planets became involved. It''s an era filled with danger, deception and intrigue where Cassian will embark on the path that is destined to turn him into a rebel hero.'
+WHERE conid = 16;
---
+++ add_weekly_boxoffice.sql
@@ -0,0 +1,81 @@
+UPDATE Movies
+SET first_4_week_box_office_domestic = ARRAY[41082703,19902083,11621807,7925965]
+WHERE conid = 1;
+
+UPDATE Movies
+SET first_4_week_box_office_domestic = ARRAY[83438667,35407034,23677389,9961518]
+WHERE conid = 2;
+
+UPDATE Movies
+SET first_4_week_box_office_domestic = ARRAY[187271136,66849562,34326571,20016782]
+WHERE conid = 3;
+
+UPDATE Movies
+SET first_4_week_box_office_domestic = ARRAY[747080,1640098,9121763,10009949]
+WHERE conid = 4;
+
+UPDATE Movies
+SET first_4_week_box_office_domestic = ARRAY[15357353,9155498,5222382,3471635]
+WHERE conid = 5;
+
+UPDATE Movies
+SET first_4_week_box_office_domestic = ARRAY[33901882,17274970,10533020,8153937]
+WHERE conid = 6;
+
+-- UPDATE Movies
+-- SET first_4_week_box_office_domestic = ARRAY[None]
+-- WHERE conid = 7;
+
+UPDATE Movies
+SET first_4_week_box_office_domestic = ARRAY[48320011,31802691,18477709,13475862]
+WHERE conid = 8;
+
+UPDATE Movies
+SET first_4_week_box_office_domestic = ARRAY[9068234,4037331,1257219,329688]
+WHERE conid = 9;
+
+-- UPDATE Movies
+-- SET first_4_week_box_office_domestic = ARRAY[None]
+-- WHERE conid = 10;
+-- 
+-- UPDATE Movies
+-- SET first_4_week_box_office_domestic = ARRAY[None]
+-- WHERE conid = 11;
+
+UPDATE Movies
+SET first_4_week_box_office_domestic = ARRAY[33083030,27081225,16057290,4644210]
+WHERE conid = 12;
+
+UPDATE Movies
+SET first_4_week_box_office_domestic = ARRAY[433935,18645663,17304189,14460655]
+WHERE conid = 25;
+
+UPDATE Movies
+SET first_4_week_box_office_domestic = ARRAY[941404,4550044,18503730,12554659]
+WHERE conid = 26;
+
+UPDATE Movies
+SET first_4_week_box_office_domestic = ARRAY[15791559,34103555,19691437,13190047]
+WHERE conid = 27;
+
+UPDATE Movies
+SET first_4_week_box_office_domestic = ARRAY[615167,1785641,2642572,3520007]
+WHERE conid = 28;
+
+UPDATE Movies
+SET first_4_week_box_office_domestic = ARRAY[14489201,10266228,7286051,4563368]
+WHERE conid = 29;
+
+UPDATE Movies
+SET first_4_week_box_office_domestic = ARRAY[19939246,55022675,26101952,18079059]
+WHERE conid = 30;
+
+UPDATE Movies
+SET first_4_week_box_office_domestic = ARRAY[44848740,30005870,21081315,13981540]
+WHERE conid = 31;
+
+UPDATE Movies
+SET first_4_week_box_office_domestic = ARRAY[39438425,48049055,21591240,13479300]
+WHERE conid = 32;
+
+
